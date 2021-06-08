236653aef4c3fd2093480d37095792fe700f868b
engincanik <engincanik@gmail.com>
2021-06-08 21:29:26 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/engin/movies_and_shows/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.engin.movies_and_shows
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movies and Shows',
+      home: MyHomePage(title: 'Movies and Shows'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Image.network(
+            'https://avatars.githubusercontent.com/u/15329674?v=4',
+          )
+        ],
+      ),
+    );
+  }
+}
